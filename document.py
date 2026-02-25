e4cef699b2715f143b006a68ae1021b3c99e344c
Mamour-D <mamourlayedione@gmail.com>
2026-02-25 09:51:10 +0000

    voici la persistance de mon mini projet qui parle sur la gestion d'un bibliothete avec la programmation orienter objet de python en y ajoutant un peu de securite avec l'encaptulation des donnees

--- document.py
+++ document.py
@@ -1,15 +1,26 @@
 from abc import ABC, abstractmethod
 
-class Document(ABC):
-    """Classe abstraite représentant un document de la bibliothèque."""
 
-    def __init__(self, titre):
+class Document(ABC):
+    """Classe abstraite représentant un document."""
+
+    def __init__(self, titre, type_doc, disponible=True, doc_id=None):
+        self._id = doc_id
         self._titre = titre
-        self.__disponible = True #(name mangling)
+        self._type_doc = type_doc
+        self.__disponible = disponible 
+
+    @property
+    def id(self):
+        return self._id
 
     @property
     def titre(self):
         return self._titre
+
+    @property
+    def type_doc(self):
+        return self._type_doc
 
     @property
     def disponible(self):
@@ -17,14 +28,11 @@ class Document(ABC):
 
     @abstractmethod
     def emprunter(self):
-        """Méthode abstraite à implémenter par les sous-classes."""
         pass
 
     @abstractmethod
     def retourner(self):
-        """Méthode abstraite à implémenter par les sous-classes."""
         pass
 
     def _changer_disponibilite(self, etat):
-        """Méthode protégée pour changer la disponibilité du document."""
         self.__disponible = etat
